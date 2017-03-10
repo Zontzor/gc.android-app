f74c3b3f7e4da32f707a2297a00320250d7422ba
Zontzip <zontzor@gmail.com>
2017-03-10 00:25:45 +0000

    Update fragments refactor

--- app/src/main/java/com/example/alex/glucosecoach/activities/MainActivity.java
+++ app/src/main/java/com/example/alex/glucosecoach/activities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.alex.glucosecoach.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,30 +18,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.alex.glucosecoach.R;
+import com.example.alex.glucosecoach.fragments.HomeFragment;
 import com.sa90.materialarcmenu.ArcMenu;
 import com.sa90.materialarcmenu.StateChangeListener;
 
 import static com.example.alex.glucosecoach.R.id.arcMenu;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private ArcMenu _arcMenu;
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private View navHeader;
+    private ImageView imgNavHeaderBg, imgProfile;
+    private TextView txtName, txtWebsite;
+    private Toolbar toolbar;
+    private ArcMenu fab;
     private FloatingActionButton _bgMenuItem, _insMenuItem, _carbsMenuItem, _exerMenuItem;
     private FrameLayout _fabFrame;
 
-    final Context _context = this;
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
+
+    // tags used to attach the fragments
+    private static final String TAG_HOME = "home";
+    private static final String TAG_LOGBOOK = "logbook";
+    private static final String TAG_CHARTS = "charts";
+    private static final String TAG_GOALS = "goals";
+    private static final String TAG_SETTINGS = "settings";
+    public static String CURRENT_TAG = TAG_HOME;
+
+    // toolbar titles respected to selected nav menu item
+    private String[] activityTitles;
+
+    // flag to load home fragment when user presses back key
+    private boolean shouldLoadHomeFragOnBackPress = true;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        _arcMenu = (ArcMenu) findViewById(arcMenu);
+        mHandler = new Handler();
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        fab = (ArcMenu) findViewById(arcMenu);
+        _fabFrame = (FrameLayout) findViewById(R.id.frame_fab);
         setupFabListener();
 
         // Setup FAB buttons
@@ -49,28 +79,214 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         _exerMenuItem = (FloatingActionButton) findViewById(R.id.fab_menu_item4_exercise);
         setupFabMenuItemsListeners();
 
-        _fabFrame = (FrameLayout) findViewById(R.id.frame_fab);
+        // Navigation view header
+        navHeader = navigationView.getHeaderView(0);
+        txtName = (TextView) navHeader.findViewById(R.id.txt_header_username);
+        txtWebsite = (TextView) navHeader.findViewById(R.id.txt_header_email);
+        imgProfile = (ImageView) navHeader.findViewById(R.id.img_header_profile);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        // load toolbar titles from string resources
+        activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // load nav menu header data
+        loadNavHeader();
+
+        // initializing navigation menu
+        setUpNavigationView();
+
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_HOME;
+            loadHomeFragment();
+        }
     }
 
+    /***
+     * Load navigation menu header information
+     * like background image, profile image
+     * name, website, notifications action view (dot)
+     */
+    private void loadNavHeader() {
+        // name, website
+        txtName.setText("Alex Kiernan");
+        txtWebsite.setText("test@test.com");
+    }
 
+    /***
+     * Returns respected fragment that user
+     * selected from navigation menu
+     */
+    private void loadHomeFragment() {
+        // selecting appropriate nav menu item
+        selectNavMenu();
+
+        // set toolbar title
+        setToolbarTitle();
+
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation drawer
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawer.closeDrawers();
+
+            // show or hide the fab button
+            toggleFab();
+            return;
+        }
+
+        // Load fragment in seperate thread
+        Runnable mPendingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // update the main content by replacing fragments
+                Fragment fragment = getHomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        };
+
+        mHandler.post(mPendingRunnable);
+
+        // show or hide the fab button
+        toggleFab();
+
+        //Closing drawer on item click
+        drawer.closeDrawers();
+
+        // refresh toolbar menu
+        invalidateOptionsMenu();
+    }
+
+    private Fragment getHomeFragment() {
+        switch (navItemIndex) {
+            case 0:
+                // home
+                HomeFragment homeFragment = new HomeFragment();
+                return homeFragment;
+            /*case 1:
+                // photos
+                PhotosFragment photosFragment = new PhotosFragment();
+                return photosFragment;
+            case 2:
+                // movies fragment
+                MoviesFragment moviesFragment = new MoviesFragment();
+                return moviesFragment;
+            case 3:
+                // notifications fragment
+                NotificationsFragment notificationsFragment = new NotificationsFragment();
+                return notificationsFragment;
+
+            case 4:
+                // settings fragment
+                SettingsFragment settingsFragment = new SettingsFragment();
+                return settingsFragment;*/
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    private void setToolbarTitle() {
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+    }
+
+    private void selectNavMenu() {
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+    }
+
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_home:
+                        navItemIndex = 0;
+                        CURRENT_TAG = TAG_HOME;
+                        break;/*
+                    case R.id.nav_photos:
+                        navItemIndex = 1;
+                        CURRENT_TAG = TAG_PHOTOS;
+                        break;
+                    case R.id.nav_movies:
+                        navItemIndex = 2;
+                        CURRENT_TAG = TAG_MOVIES;
+                        break;
+                    case R.id.nav_notifications:
+                        navItemIndex = 3;
+                        CURRENT_TAG = TAG_NOTIFICATIONS;
+                        break;
+                    case R.id.nav_settings:
+                        navItemIndex = 4;
+                        CURRENT_TAG = TAG_SETTINGS;
+                        break;*/
+                    default:
+                        navItemIndex = 0;
+                }
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+                loadHomeFragment();
+
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+    }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+            drawer.closeDrawers();
+            return;
         }
+
+        // This code loads home fragment when back key is pressed
+        // when user is in other fragment than home
+        if (shouldLoadHomeFragOnBackPress) {
+            // checking if user is on other navigation menu
+            // rather than home
+            if (navItemIndex != 0) {
+                navItemIndex = 0;
+                CURRENT_TAG = TAG_HOME;
+                loadHomeFragment();
+                return;
+            }
+        }
+
+        super.onBackPressed();
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -91,7 +307,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_signout) {
-            Intent intent = new Intent(_context, LoginActivity.class);
+            Intent intent = new Intent(this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
         }
@@ -101,16 +317,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+
+
     public void setupFabListener() {
-        _arcMenu.setStateChangeListener(new StateChangeListener() {
+        fab.setStateChangeListener(new StateChangeListener() {
             @Override
             public void onMenuOpened() {
-                _fabFrame.setBackgroundColor(ContextCompat.getColor(_context, R.color.transparentGray));
+                _fabFrame.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.transparentGray));
                 _fabFrame.setOnTouchListener(new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View view, MotionEvent motionEvent) {
-                        if (_arcMenu.isMenuOpened()) {
-                            _arcMenu.toggleMenu();
+                        if (fab.isMenuOpened()) {
+                            fab.toggleMenu();
                         }
 
                         return true;
@@ -129,9 +347,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         _bgMenuItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (_arcMenu.isMenuOpened()) {
-                    _arcMenu.toggleMenu();
-                }
                 Intent addBGReadingActivity = new Intent(MainActivity.this, AddBGReadingActivity.class);
                 startActivity(addBGReadingActivity);
             }
@@ -140,9 +355,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         _insMenuItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (_arcMenu.isMenuOpened()) {
-                    _arcMenu.toggleMenu();
-                }
                 Intent addInsulinActivity = new Intent(MainActivity.this, AddInsulinActivity.class);
                 startActivity(addInsulinActivity);
             }
@@ -151,9 +363,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         _carbsMenuItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (_arcMenu.isMenuOpened()) {
-                    _arcMenu.toggleMenu();
-                }
                 Intent addCarbsActivity = new Intent(MainActivity.this, AddFoodLogActivity.class);
                 startActivity(addCarbsActivity);
             }
@@ -162,12 +371,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         _exerMenuItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (_arcMenu.isMenuOpened()) {
-                    _arcMenu.toggleMenu();
-                }
                 Intent addExerciseActivity = new Intent(MainActivity.this, AddExerciseLogActivity.class);
                 startActivity(addExerciseActivity);
             }
         });
+    }
+
+    // show or hide the fab
+    private void toggleFab() {
+        if (fab.isMenuOpened()) {
+            fab.toggleMenu();
+        }
     }
 }
---
+++ app/src/main/java/com/example/alex/glucosecoach/fragments/dep.java
@@ -0,0 +1,221 @@
+package com.example.alex.glucosecoach.fragments;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.alex.glucosecoach.R;
+import com.example.alex.glucosecoach.activities.LoginActivity;
+import com.example.alex.glucosecoach.controller.ApiManager;
+import com.example.alex.glucosecoach.controller.TokenManager;
+import com.example.alex.glucosecoach.controller.UserManager;
+import com.example.alex.glucosecoach.models.Fact;
+import com.example.alex.glucosecoach.models.InsValue;
+import com.example.alex.glucosecoach.models.User;
+import com.example.alex.glucosecoach.services.FactService;
+import com.example.alex.glucosecoach.services.InsService;
+import com.example.alex.glucosecoach.services.PredictionService;
+import com.example.alex.glucosecoach.services.UserService;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by alex on 3/9/17.
+ */
+
+public class dep extends AppCompatActivity {
+    private TextView _txtBGValue, _txtInsulinValue, _txtCarbsValue, _txtExerciseValue;
+    private Button _btnStartPredictionActivity;
+    final Context _context = this;
+
+    ApiManager _apiManager;
+    TokenManager _tokenManager;
+    UserManager _userManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        _tokenManager = new TokenManager(_context);
+        _apiManager = new ApiManager(_tokenManager.getToken());
+        _userManager = new UserManager(_context);
+
+        if (isLoggedIn()) {
+            _userManager = new UserManager(_context);
+            loadContent();
+        } else {
+            startLoginActivity();
+        }
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        populateMainScreen();
+    }
+
+    public void loadContent() {
+
+
+
+        // Setup Main Screen Text Views
+        _txtBGValue = (TextView) findViewById(R.id.txt_last_bg_value);
+        _txtInsulinValue = (TextView) findViewById(R.id.txt_last_ins_value);
+        _txtCarbsValue = (TextView) findViewById(R.id.txt_last_carbs_value);
+        _txtExerciseValue = (TextView) findViewById(R.id.txt_last_exrc_value);
+
+        // Create API Manager instance
+        _apiManager = new ApiManager(_tokenManager.getToken());
+
+        _btnStartPredictionActivity = (Button) findViewById(R.id.btn_start_predicition);
+        _btnStartPredictionActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FactService factService = _apiManager.getFactService();
+                Call<Fact> call = factService.getFact(_userManager.getUsername());
+                call.enqueue(new Callback<Fact>() {
+                    @Override
+                    public void onResponse(Call<Fact> call, Response<Fact> response) {
+                        if (response.isSuccessful()) {
+                            Fact fact = response.body();
+
+                            PredictionService predictionService = _apiManager.getPredictionService();
+                            Call<Double> call2 = predictionService.getPrediction(fact, _userManager.getUsername());
+                            call2.enqueue(new Callback<Double>() {
+                                @Override
+                                public void onResponse(Call<Double> call, final Response<Double> response) {
+                                    if (response.isSuccessful()) {
+                                        new AlertDialog.Builder(_context)
+                                                .setTitle("Recommended Insulin")
+                                                .setMessage(response.body().toString())
+                                                .setNegativeButton(android.R.string.cancel, null) // dismisses by default
+                                                .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                                                    @Override public void onClick(DialogInterface dialog, int which) {
+                                                        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                                                        Date date = new Date();
+
+                                                        InsValue insValue = new InsValue("novorapid", response.body(), dateFormat.format(date));
+
+                                                        InsService insService = _apiManager.getInsService();
+                                                        Call<InsValue> call = insService.postInsDosage(insValue, _userManager.getUsername());
+                                                        call.enqueue(new Callback<InsValue >() {
+                                                            @Override
+                                                            public void onResponse(Call<InsValue> call, Response<InsValue> response) {
+
+                                                                if (response.isSuccessful()) {
+                                                                    Log.d("insvalue", "Successful post");
+                                                                    populateMainScreen();
+                                                                } else {
+                                                                    Log.d("insvalue", "Unsuccessful post");
+                                                                }
+                                                            }
+
+                                                            @Override
+                                                            public void onFailure(Call<InsValue> call, Throwable t) {
+                                                                Log.d("Error", t.getMessage());
+                                                            }
+                                                        });
+                                                    }
+                                                })
+                                                .create()
+                                                .show();
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Call<Double> call, Throwable t) {
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Fact> call, Throwable t) {
+                    }
+                });
+            }
+        });
+
+        // Get user info from API and fill in side menu info
+        getUser();
+
+        populateMainScreen();
+    }
+
+    public boolean isLoggedIn() {
+        if (!_tokenManager.hasToken()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void getUser() {
+        UserService userService = _apiManager.getUserService();
+        Call<User> call = userService.getUser(_userManager.getUsername());
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    TextView _usernameText = (TextView) findViewById(R.id.txt_header_username);
+                    TextView _emailText = (TextView) findViewById(R.id.txt_header_email);
+
+                    _usernameText.setText(response.body().getUsername());
+                    _emailText.setText(response.body().getEmail());
+
+                    _userManager.setEmail(response.body().getEmail());
+                } else {
+                    // error response, no access to resource?
+                    Log.d("authentication", "Incorrect login details");
+                    startLoginActivity();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                // something went completely south (like no internet connection)
+            }
+        });
+    }
+
+    public void populateMainScreen() {
+        // Populate main screen with data
+        FactService factService = _apiManager.getFactService();
+        Call<Fact> call = factService.getFact(_userManager.getUsername());
+        call.enqueue(new Callback<Fact>() {
+            @Override
+            public void onResponse(Call<Fact> call, Response<Fact> response) {
+                if (response.isSuccessful()) {
+                    Fact fact = response.body();
+                    _txtBGValue.setText(getString(R.string.main_bg, fact.getBgValue().toString()));
+                    _txtInsulinValue.setText(getString(R.string.main_ins, fact.getInsValue().toString()));
+                    _txtCarbsValue.setText(getString(R.string.main_carbs, fact.getFoodValue().toString()));
+                    _txtExerciseValue.setText(getString(R.string.main_exer, fact.getExerciseValue().toString()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Fact> call, Throwable t) {
+            }
+        });
+    }
+
+    public void startLoginActivity() {
+        Intent loginActivity = new Intent(this, LoginActivity.class);
+        loginActivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(loginActivity);
+    }
+}
