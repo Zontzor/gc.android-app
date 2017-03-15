977930f7b5e6938918f8aa12de02461050b952f0
Zontzip <zontzor@gmail.com>
2017-03-15 08:33:42 +0000

    Update ip address of api

--- app/src/main/java/com/example/alex/glucosecoach/controller/ApiManager.java
+++ app/src/main/java/com/example/alex/glucosecoach/controller/ApiManager.java
@@ -23,7 +23,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiManager {
 
-    private static String BASE_URL = "http://192.168.1.101:5000/glucose_coach/api/v1.0/";
+    private static String BASE_URL = "http://ec2-34-248-62-100.eu-west-1.compute.amazonaws.com:5000/glucose_coach/api/v1.0/";
 
     private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
